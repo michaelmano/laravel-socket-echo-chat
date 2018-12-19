67d88f2f41e1082bb85b8d71a364e3dce18fe3ee
Michael Mano <michael.mano26@gmail.com>
2018-12-19 14:30:46 +1000

    Add vue compilers and add example component

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,5 @@
-
+import Echo from 'laravel-echo';
+window.io = require('socket.io-client');
 window._ = require('lodash');
 
 /**
@@ -35,7 +36,9 @@ let token = document.head.querySelector('meta[name="csrf-token"]');
 if (token) {
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+    console.error(
+        'CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token',
+    );
 }
 
 /**
@@ -44,13 +47,9 @@ if (token) {
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo'
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     encrypted: true
-// });
+if (typeof io !== 'undefined') {
+    window.Echo = new Echo({
+        broadcaster: 'socket.io',
+        host: window.location.hostname + ':6001',
+    });
+}
---
+++ resources/js/components/ExampleChat.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default mb-4">
+                    <div class="card-header">Messages</div>
+                    <div class="px-2 py-1">
+                        <p v-for="(message, index) in messages" :key="index">{{ message }}</p>
+                    </div>
+                </div>
+                <div class="card card-default">
+                    <input v-model="text">
+                    <button @click="postMessage" :disabled="!contentExists">submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+ </template>
+
+  <script>
+ export default {
+         data() {
+             return {
+                 text: '',
+                 messages: []
+             }
+         },
+         computed: {
+             contentExists() {
+                 return this.text.length > 0;
+             }
+         },
+         methods: {
+             postMessage() {
+                 axios.post('/post', {message: this.text}).then(({data}) => {
+                     console.log(data)
+                     this.messages.push(this.text);
+                     this.text = '';
+                 });
+             },
+         },
+         created() {
+             Echo.channel('chat').listen('ChatMessage', ({message}) => {
+                 this.messages.push(message);
+             });
+         }
+ }
+ </script>
