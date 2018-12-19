7866ccc6f1bf1aa45d34e91e4903fcd4e26b9107
Michael Mano <michael.mano26@gmail.com>
2018-12-19 16:08:13 +1000

    Remove port from echo for production

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -50,6 +50,6 @@ if (token) {
 if (typeof io !== 'undefined') {
     window.Echo = new Echo({
         broadcaster: 'socket.io',
-        host: window.location.hostname + ':6001',
+        host: window.location.hostname,
     });
 }
